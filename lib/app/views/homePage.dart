65047dcdee4805f6af8a654b2e752065b8eb3529
Jayant Choudhary <jayant2204@gmail.com>
2020-09-23 01:40:46 +0530

    Themes Support

--- lib/app/views/homePage.dart
+++ lib/app/views/homePage.dart
@@ -14,6 +14,7 @@ class HomePage extends StatelessWidget {
         title: Text('Simple Todo List'),
         centerTitle: true,
         actions: [
+          // Theme Changer added
           IconButton(
             icon: Icon(Icons.wb_sunny),
             onPressed: () {
